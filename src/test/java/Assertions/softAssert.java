57b715e7abb7ae10f40b284a43eb6672d8bae5b8
Aslam8989 <kmohdaslam8989@gmail.com>
2025-02-27 10:24:08 +0530

    Update softAssert.java

--- src/test/java/Assertions/softAssert.java
+++ src/test/java/Assertions/softAssert.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
+//Changes done in github
 public class softAssert {
 	@Test
 	public void SoftAssert() {
